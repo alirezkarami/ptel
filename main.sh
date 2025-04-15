78524d53ca5aa01208c91917752c31bba4e18265
alireza <69981338+alirezkarami@users.noreply.github.com>
2025-04-15 21:46:16 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# مسیر اسکریپت فعلی
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 PORT=${1:-443}
 CUSTOM_TAG=${2:-b0cbcef5a486d9636482ac27f8e46a9d}
 TELEGRAM_TAG=$3
@@ -10,4 +13,5 @@ if [[ -z "$TELEGRAM_TAG" ]]; then
     exit 1
 fi
 
-bash mtp_install.sh -p "$PORT" -s "$CUSTOM_TAG" -t "$TELEGRAM_TAG" -a dd -a tls -d "$TLS_DOMAIN"
+# اجرای mtp_install.sh از همسایگی
+bash "$SCRIPT_DIR/mtp_install.sh" -p "$PORT" -s "$CUSTOM_TAG" -t "$TELEGRAM_TAG" -a dd -a tls -d "$TLS_DOMAIN"
